11f6bcf757f7dcf13ad488e08eb706c5aa0069de
Fymyte <pierguill@gmail.com>
2021-12-27 16:09:29 +0100

    fix(colors): fix background colors
    Signed-off-by: Fymyte <pierguill@gmail.com>

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -1,3 +1,14 @@
+local colors = {
+  sumiInk1 = "#212121",
+  sumiInk0 = "#2A2A2A",
+  sumiInk2 = "#353535",
+  sumiInk3 = "#414141",
+  sumiInk4 = "#5F5F5F",
+
+  waveBlue1 = "#2F2F2F",
+  waveBlue2 = "#393939",
+}
+
 require('kanagawa').setup({
     undercurl = true,           -- enable undercurls
     commentStyle = "italic",
@@ -7,9 +18,9 @@ require('kanagawa').setup({
     typeStyle = "NONE",
     variablebuiltinStyle = "italic",
     specialReturn = true,       -- special highlight for the return keyword
-    specialException = true,    -- special highlight for exception handling keywords 
+    specialException = true,    -- special highlight for exception handling keywords
     transparent = false,        -- do not set background color
-    colors = {},
+    colors = colors,
     overrides = {},
 })
 
