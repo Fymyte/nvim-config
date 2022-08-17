6e529eda6b47521cd9490074b71fea783a310458
Pierrick Guillaume <pierguill@gmail.com>
2022-08-17 14:55:43 -0400

    ref(plugin): remove fidget for ltex

--- after/plugin/fidget.lua
+++ after/plugin/fidget.lua
@@ -2,4 +2,10 @@ require"fidget".setup {
   text = { spinner = "dots_pulse" },
   window = { relative = "editor" },
   timer = { fidget_decay = 3000 },
+  sources = {
+    ['ltex'] = { ignore = true },
+  },
+  fmt = {
+    max_width = 32
+  }
 }
