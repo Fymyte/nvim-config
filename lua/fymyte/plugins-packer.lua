e7da3307ca321b63f9de3c7d4813e9948b249411
Pierrick Guillaume <pierguill@gmail.com>
2023-02-01 00:13:08 +0100

    cleanup packer plugins

--- lua/fymyte/plugins-packer.lua
+++ lua/fymyte/plugins-packer.lua
@@ -23,27 +23,6 @@ packer.startup {
     --   run = check_system_deps({ 'himalaya' }, 'himalaya')
     -- }
 
-    -- Telescope (Fuzzy finder)
-
-    -- Autocompletion
-    use {
-      'hrsh7th/nvim-cmp',
-      requires = {
-        'hrsh7th/cmp-nvim-lsp',
-        'hrsh7th/cmp-nvim-lsp-signature-help',
-        'hrsh7th/cmp-nvim-lsp-document-symbol',
-        'hrsh7th/cmp-buffer',
-        'hrsh7th/cmp-path',
-        'hrsh7th/cmp-cmdline',
-        'hrsh7th/cmp-nvim-lua',
-        'f3fora/cmp-spell',
-        'saadparwaiz1/cmp_luasnip',
-        'L3MON4D3/LuaSnip',
-        { 'petertriho/cmp-git', requires = 'nvim-lua/plenary.nvim' },
-        'rcarriga/cmp-dap',
-      },
-    }
-
     -- LSP
     use 'neovim/nvim-lspconfig'
     use 'williamboman/mason.nvim'
@@ -68,12 +47,6 @@ packer.startup {
     --   'nvim-treesitter/nvim-treesitter-textobjects',
     --   after = 'nvim-treesitter'
     -- }
-    -- UI
-    use {
-      'onsails/lspkind-nvim',
-      requires = { 'neovim/nvim-lspconfig' },
-    }
-    -- use { 'mhinz/vim-startify' }
   end,
   config = {
     display = {
