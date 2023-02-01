b163489ef0494c55fe67165099137ab43da62e86
Pierrick Guillaume <pguillaume@sequans.com>
2023-02-01 16:58:39 +0100

    use noice.nvim

--- lua/fymyte/plugins/ui.lua
+++ lua/fymyte/plugins/ui.lua
@@ -78,4 +78,23 @@ return {
       },
     },
   },
+
+  -- Noicer cmd
+  {
+    'folke/noice.nvim',
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
+    },
+    opts = {
+      lsp = {
+        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true,
+        },
+      },
+    },
+  },
 }
