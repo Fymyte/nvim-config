2c97f3cc8599ed67e2aaba758d2d696e25674c75
Pierrick Guillaume <pguillaume@sequans.com>
2023-02-15 15:24:23 +0100

    fix: run rust-analyzer using rustup

--- lua/fymyte/tools/lsp.lua
+++ lua/fymyte/tools/lsp.lua
@@ -138,10 +138,16 @@ local ltex_languages = {
 ---@type ServerConfigs
 M.servers = {
   ['rust_analyzer'] = function()
+    local rust_analyzer_cmd = { "rustup", "run", "nightly", "rust-analyzer" }
     local extension_path = vim.fn.stdpath 'data' .. '/mason/packages/codelldb/extension'
     local codelldb_path = extension_path .. '/adapter/codelldb'
     local liblldb_path = extension_path .. '/lldb/lib/liblldb.so'
+
     require('rust-tools').setup {
+      server = {
+        -- `on_attach` and `capabilities` are set using default values
+        cmd = rust_analyzer_cmd,
+      },
       dap = {
         adapter = require('rust-tools.dap').get_codelldb_adapter(codelldb_path, liblldb_path),
       },
