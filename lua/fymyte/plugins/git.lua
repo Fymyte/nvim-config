d3e40dd351d933e2fded7bc3f68001cfaf50b7f5
Pierrick Guillaume <pguillaume@sequans.com>
2023-02-27 11:19:24 +0100

    fix: gitsigns on_attach

--- lua/fymyte/plugins/git.lua
+++ lua/fymyte/plugins/git.lua
@@ -27,7 +27,7 @@ return {
         local gs = require 'gitsigns'
         local opts = { noremap = true, silent = true }
         local function map(mode, l, r, lopts)
-          lopts = vim.tbl_extend('force', opts, lopts)
+          lopts = vim.tbl_extend('force', opts, lopts or {})
           lopts.buffer = bufnr
           vim.keymap.set(mode, l, r, lopts)
         end
@@ -56,9 +56,9 @@ return {
         map({ 'n', 'v' }, '<leader>hs', gs.stage_hunk, { desc = '[H]unk [S]tage' })
         map({ 'n', 'v' }, '<leader>hr', gs.reset_hunk, { desc = '[H]unk [R]eset' })
         map('n', '<leader>hS', gs.stage_buffer, { desc = '[H]unk [S]tage buffer' })
-        map('n', '<leader>hu', gs.undo_stage_hunk { desc = '[H]unk [U]ndo stage' })
+        map('n', '<leader>hu', gs.undo_stage_hunk, { desc = '[H]unk [U]ndo stage' })
         map('n', '<leader>hR', gs.reset_buffer, { desc = '[H]unk [R]eset buffer' })
-        map('n', '<leader>hp', gs.preview_hunk { desc = '[H]unk [P]review' })
+        map('n', '<leader>hp', gs.preview_hunk, { desc = '[H]unk [P]review' })
         map('n', '<leader>hb', function()
           gs.blame_line { full = true }
         end, { desc = '[H]unk [B]lame' })
