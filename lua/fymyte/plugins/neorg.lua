4dadc24780218f9fcd41e2dda82ea532263983a2
Pierrick Guillaume <pguillaume@sequans.com>
2024-05-21 16:36:25 +0200

    ref(neorg): update to 0.8

--- lua/fymyte/plugins/neorg.lua
+++ lua/fymyte/plugins/neorg.lua
@@ -1,57 +1,61 @@
 return {
-  'nvim-neorg/neorg',
-  build = ':Neorg sync-parsers',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'nvim-neorg/neorg-telescope',
-    'hrsh7th/nvim-cmp',
+  {
+    "vhyrro/luarocks.nvim",
+    priority = 1000,
+    config = true,
   },
-  opts = {
-    load = {
-      ['core.defaults'] = {}, -- Load all the default modules
-      ['core.keybinds'] = {
-        config = {
-          hook = function(keybinds)
-            keybinds.unmap('norg', 'n', '<M-j>')
-            keybinds.unmap('norg', 'n', '<M-k>')
-          end,
-        },
-      },
-      ['core.concealer'] = {
-        config = {
-          icon_preset = 'diamond',
-        },
-      }, -- Allows for use of icons
-      ['core.qol.toc'] = {},
-      -- ["core.gtd.base"] = {
-      --   config = {
-      --     workspace = "gtd",
-      --   }
-      -- },
-      ['core.dirman'] = { -- Manage your directories with Neorg
-        config = {
-          workspaces = {
-            notes = '~/Documents/notes/',
-            work = '~/Documents/dev/doc/notes/',
-            mails = '~/.cache/neorg/mails'
-            -- gtd = "~/Documents/notes/gtd",
+  {
+    "nvim-neorg/neorg",
+    dependencies = { "luarocks.nvim" },
+    lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
+    version = "*", -- Pin Neorg to the latest stable release
+    opts = {
+      load = {
+        ['core.defaults'] = {}, -- Load all the default modules
+        ['core.keybinds'] = {
+          config = {
+            hook = function(keybinds)
+              keybinds.unmap('norg', 'n', '<M-j>')
+              keybinds.unmap('norg', 'n', '<M-k>')
+            end,
           },
-          default_workspace = 'work',
         },
-      },
-      ['core.completion'] = {
-        config = {
-          engine = 'nvim-cmp', -- We current support nvim-compe and nvim-cmp only
+        ['core.concealer'] = {
+          config = {
+            icon_preset = 'diamond',
+          },
+        }, -- Allows for use of icons
+        ['core.qol.toc'] = {},
+        -- ["core.gtd.base"] = {
+        --   config = {
+        --     workspace = "gtd",
+        --   }
+        -- },
+        ['core.dirman'] = { -- Manage your directories with Neorg
+          config = {
+            workspaces = {
+              notes = '~/Documents/notes/',
+              work = '~/Documents/dev/doc/notes/',
+              mails = '~/.cache/neorg/mails'
+              -- gtd = "~/Documents/notes/gtd",
+            },
+            default_workspace = 'work',
+          },
         },
-      },
-      ['core.export'] = {},
-      ['core.export.markdown'] = {},
-      ['core.presenter'] = {
-        config = {
-          zen_mode = 'truezen',
+        ['core.completion'] = {
+          config = {
+            engine = 'nvim-cmp', -- We current support nvim-compe and nvim-cmp only
+          },
         },
+        ['core.export'] = {},
+        ['core.export.markdown'] = {},
+        ['core.presenter'] = {
+          config = {
+            zen_mode = 'truezen',
+          },
+        },
+        -- ['core.integrations.telescope'] = {},
       },
-      ['core.integrations.telescope'] = {},
     },
-  },
+  }
 }
