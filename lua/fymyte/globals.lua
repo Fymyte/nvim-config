41a20612e3e51ecac3f6e9e00b6977a6921eb2d2
Pierrick Guillaume <pguillaume@sequans.com>
2024-06-14 15:17:42 +0200

    ref: better utils

--- lua/fymyte/globals.lua
+++ lua/fymyte/globals.lua
@@ -2,6 +2,10 @@
 
 --# selene: allow(incorrect_standard_library_use)
 
+--- Identity function, used to debug a value.
+--- This is usefull to easily print the content of a table
+--- @param v any Any variable which can be printed
+---@return any v
 P = function(v)
   print(vim.inspect(v))
   return v
--- lua/fymyte/utils.lua
+++ lua/fymyte/utils.lua
@@ -84,50 +84,18 @@ end
 M.autocmd = vim.api.nvim_create_autocmd
 
 ---Check if dependencies are installed and executable on the system
----@param deps SystemDep
+---@param deps SystemDeps[]
 ---@param name string
 local function check_system_deps(deps, name)
-  for _, dep in pairs(deps) do
-    if vim.fn.executable(dep) ~= 1 then
-      local msg = ('%q is not installed'):format(dep)
-      vim.notify(msg, 'warn', { title = name })
-    end
-  end
+  local missing = system_deps(deps)
+  if #missing == 0 then return end
+
+  local msg
+  if #missing > 1 then msg = 'are' else msg = 'is' end
+
+  msg = ('%q %s not installed'):format(missing, msg)
+  vim.notify(msg, vim.log.levels.WARN, { title = name })
 end
-
-local log_levels = { ['TRACE'] = 0, ['DEBUG'] = 1, ['INFO'] = 2, ['WARN'] = 3, ['ERROR'] = 4 }
-vim.tbl_add_reverse_lookup(log_levels)
-
---- Log as a notification only when `vim.g.log_level`
---- is greater than `min_lvl`
---- @param min_lvl number | string
---- @param msg string
-function M.log(min_lvl, msg)
-  local log_level = g.log_level
-  if type(min_lvl) == 'string' then
-    min_lvl = assert(log_levels[min_lvl:upper()], string.format('bad log level: %q', min_lvl))
-  end
-  if type(log_level) == 'string' then
-    log_level = assert(log_levels[log_level:upper()], string.format('bad main log level: %q', log_level))
-  end
-  assert(type(min_lvl) == 'number' and type(log_level) == 'number', 'log levels are either string or number')
-  if log_level <= min_lvl then
-    log_level = string.lower(log_levels[min_lvl])
-    vim.notify(msg, log_level, { title = log_level })
-  end
-end
-
--- local function generic_map(key, map_fun)
---   -- get the extra options
---   local opts = { noremap = true, silent = true }
---   for i, v in pairs(key) do
---     if type(i) == 'string' then
---       opts[i] = v
---     end
---   end
---   map_fun(key[1], key[2], key[3], opts)
---   M.log('trace', string.format('add new keymap: { mode = %q, keys: %q, cmd: %q }', key[1], key[2], key[3]))
--- end
 
 M.check_system_deps = check_system_deps
 M.system_deps = system_deps
