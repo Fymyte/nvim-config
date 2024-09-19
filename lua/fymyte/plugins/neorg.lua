00989aa1dfc7d247720b1f8ea39b30c7cbfbac6d
Pierrick Guillaume <pguillaume@sequans.com>
2024-09-19 13:23:23 +0200

    ref(neorg): cleanup plugin spec

--- lua/fymyte/plugins/neorg.lua
+++ lua/fymyte/plugins/neorg.lua
@@ -1,12 +1,7 @@
 return {
   {
-    "vhyrro/luarocks.nvim",
-    priority = 1000,
-    config = true,
-  },
-  {
     "nvim-neorg/neorg",
-    dependencies = { "luarocks.nvim" },
+    dependencies = { "pocco81/true-zen.nvim" },
     lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
     version = "*", -- Pin Neorg to the latest stable release
     opts = {
@@ -57,5 +52,6 @@ return {
         -- ['core.integrations.telescope'] = {},
       },
     },
+    config = true,
   }
 }
