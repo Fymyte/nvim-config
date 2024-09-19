b27062f3185bc569612673c442b918fd071a79c8
Pierrick Guillaume <pguillaume@sequans.com>
2024-09-19 13:28:13 +0200

    add(snippets): lua and c snippets

---
+++ luasnippets/asm.lua
@@ -0,0 +1,17 @@
+--# selene: allow(unused_variable)
+--- @diagnostic disable:unused-local
+
+local ls = require 'luasnip'
+local s = ls.snippet
+local i = ls.insert_node
+local fmt = require('luasnip.extras.fmt').fmt
+local fmta = require('luasnip.extras.fmt').fmta
+
+return {
+  s('macro',
+    fmt([[
+    .macro {}
+    	{}
+    .endm]], { i(1), i(0) })
+  ),
+}
