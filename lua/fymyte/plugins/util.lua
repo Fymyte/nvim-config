c150bc2f136727ee945e6f6aed4f14bbd5c61f48
Pierrick Guillaume <pguillaume@sequans.com>
2024-09-26 16:09:54 +0200

    ref(plugins): update himalaya url

--- lua/fymyte/plugins/util.lua
+++ lua/fymyte/plugins/util.lua
@@ -6,7 +6,7 @@ return {
     end,
   },
   {
-    'https://git.sr.ht/~soywod/himalaya-vim',
+    'pimalaya/himalaya-vim',
     -- Use cond instead of enabled to have it as soon as himalaya is installed
     cond = function()
       return vim.fn.executable 'himalaya' == 1
