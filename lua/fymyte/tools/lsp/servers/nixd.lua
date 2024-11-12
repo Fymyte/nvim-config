d12d789aa212d94110e866389b4768639cd586a3
Pierrick Guillaume <pguillaume@sequans.com>
2024-11-12 07:01:10 +0100

    fix(tools): nixd expressions to evaluate user's options

--- lua/fymyte/tools/lsp/servers/nixd.lua
+++ lua/fymyte/tools/lsp/servers/nixd.lua
@@ -2,13 +2,13 @@ return {
   cmd = { 'nixd', '--inlay-hints=true'},
   settings = {
     nixd = {
-      nixpkgs = { expr = 'import <nixpkgs> {}' },
+      nixpkgs = { expr = '(builtins.getFlake ("git+file://" + toString ./.)).inputs.nixpkgs' },
       options = {
         nixos = {
-          expr = '(builtins.getFlake ("git+file://" + toString ./.)).nixosConfigurations.k-on.options',
+          expr = '(builtins.getFlake ("git+file://" + toString ./.)).nixosConfigurations.snorlax.options',
         },
         home_manager = {
-          expr = '(builtins.getFlake ("git+file://" + toString ./.)).homeConfigurations."ruixi@k-on".options',
+          expr = '(builtins.getFlake ("git+file://" + toString ./.)).homeConfigurations."pguillaume@snorlax".options',
         },
       },
     },
