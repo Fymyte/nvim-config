79bded19b9998b2908d47da085da1dc8805ea0fd
Pierrick Guillaume <pguillaume@sequans.com>
2024-11-19 11:14:39 +0100

    ref(tools): nil_ls: don't use semantik tokens

--- lua/fymyte/tools/lsp/servers/nil_ls.lua
+++ lua/fymyte/tools/lsp/servers/nil_ls.lua
@@ -1,5 +1,13 @@
+local default_config = require('lspconfig').util.default_config
+
 return {
   settings = {
     ['nil'] = { nix = { flake = { autoEvalInputs = false } } },
   },
+
+  on_attach = function(client, _)
+    default_config.on_attach()
+    -- Dont use semantic tockens for nil: use nixd for this
+    client.server_capabilities.semanticTokensProvider = nil
+  end,
 }
