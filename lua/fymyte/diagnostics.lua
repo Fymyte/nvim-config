3d6d58bf45f66cbe32c51dbe83b7a2b6bd6845ce
Pierrick Guillaume <pguillaume@sequans.com>
2024-12-03 16:00:36 +0100

    ref(diagnostics): don't override default symbols

--- lua/fymyte/diagnostics.lua
+++ lua/fymyte/diagnostics.lua
@@ -9,15 +9,3 @@ vim.diagnostic.config {
   severity_sort = false,
   float = { focusable = false },
 }
-
--- Change diagnostic symbols in the sign column
-local signs = {
-  Error = '',
-  Warn = '',
-  Hint = ' ',
-  Info = '',
-}
-for type, icon in pairs(signs) do
-  local hl = 'DiagnosticSign' .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-end
