8f6f17056e21dda797dc9bc9f83ee7c76268afee
Pierrick Guillaume <pguillaume@sequans.com>
2025-01-21 08:07:52 +0100

    chore: format list-plugins script

--- scripts/list-plugins.lua
+++ scripts/list-plugins.lua
@@ -2,5 +2,5 @@
 local plugins = require('lazy').plugins()
 
 for _, plugin in ipairs(plugins) do
-print(string.format('- [%s](%s)', plugin.name, plugin.url))
+  print(string.format('- [%s](%s)', plugin.name, plugin.url))
 end
