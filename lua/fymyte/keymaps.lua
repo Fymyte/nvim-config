cc366ddd8c75d3899f267a774bef884d160cfaf2
Pierrick Guillaume <pguillaume@fymyte.com>
2025-04-15 21:31:42 +0200

    ref(keymaps): fix typos and clean deprecation

--- lua/fymyte/keymaps.lua
+++ lua/fymyte/keymaps.lua
@@ -2,36 +2,39 @@
 --- Keymaps associated with plugins should be located with the plugins
 --- declaration itself in `lua/fymyte/plugins/<file>`
 
+--- Shortcut for vim.keymap.set, but always set noremap and silent to true
+--- @param mode string|string[]
+--- @param lhs string
+--- @param rhs string|function
+--- @param desc string?
 local keymap = function(mode, lhs, rhs, desc)
   vim.keymap.set(mode, lhs, rhs, { noremap = true, silent = true, desc = desc })
 end
 
 -- Quick save
 keymap('n', '<leader>w', '<cmd>write!<cr>', '[W]rite buffer')
--- Overload open
-keymap('n', 'gx', ':!open <c-r><c-a><cr>', 'Open content under cursor')
--- Do not add breakpoints when typing punctuation
+keymap('n', '<leader>q', '<cmd>quit<cr>', '[Q]uit window')
+
+-- Do not add breakpoints when typing punctuation. This allows to under writing a lua statement in a single press of `u`
+-- instead of one for every field
 keymap('i', ',', ',<C-g>u')
 keymap('i', '.', '.<C-g>u')
 keymap('i', '!', '!<C-g>u')
 keymap('i', '?', '?<C-g>u')
 keymap('i', ':', ':<C-g>u')
--- Cycle throught buffers
+
+-- Cycle through buffers
 keymap('', '<leader>bp', '<cmd>bprev<cr>', '[B]uffer [P]rev')
 keymap('', '<leader>bn', '<cmd>bnext<cr>', '[B]uffer [N]ext')
--- Cycle throught tabs
+-- Cycle through tabs
 keymap('n', '<leader>tn', '<cmd>tabnext<cr>', '[T]ab [N]ext')
 keymap('n', '<leader>tp', '<cmd>tabprev<cr>', '[T]ab [P]rev')
 keymap('n', '<leader>te', '<cmd>tabedit<cr>', '[T]ab [E]dit')
 keymap('n', '<leader>ts', '<cmd>tab split<cr>', '[T]ab [S]plit')
--- Dont move cursor when joining lines
+-- Don't move cursor when joining lines
 keymap('n', 'J', 'mzJ`z')
 -- Join line above at the end of the current line
 keymap('n', '<leader>j', [[<cmd>m-2|j<cr>]], '[J]oin line above after this line')
--- Formatting
-keymap('n', '<leader>f', function()
-  vim.lsp.buf.format { async = true }
-end, '[F]ormat buffer')
 -- Misc
 keymap('n', '<leader><leader>', '<cmd>noh<cr>', 'Clear search highlighting')
 -- Paste without losing clipboard content
@@ -47,9 +50,14 @@ keymap('o', 'i%', '<cmd>normal vi%<cr>')
 
 keymap('n', '<leader>do', vim.diagnostic.open_float, '[D]iagnostic [O]pen')
 keymap('n', '<leader>dr', vim.diagnostic.reset, '[D]iagnostic [R]eset')
-keymap('n', '[d', vim.diagnostic.goto_prev, 'Prev [D]iagnostic')
-keymap('n', ']d', vim.diagnostic.goto_next, 'Next [D]iagnostic')
+keymap('n', '[d', function() vim.diagnostic.jump { count = -1, float = true } end, 'Prev [D]iagnostic')
+keymap('n', ']d', function() vim.diagnostic.jump { count = 1, float = true } end, 'Next [D]iagnostic')
 keymap('n', '<leader>dq', vim.diagnostic.setloclist, '[D]iagnostic [Q]uick fix')
+
+keymap('n', '<leader>o', function()
+  vim.cmd.edit(vim.fn.expand '%:p:h')
+end, "[O]pen file's directory")
+keymap('n', '<leader>f', 'gq%', '[F]ormat')
 
 -- Common yanking ops
 keymap('n', '<leader>yf', function()
@@ -60,7 +68,8 @@ keymap('n', '<leader>yp', function()
 end, '[Y]ank [P]ath to current file')
 
 keymap('t', '<S-esc>', '<C-\\><C-n>', 'Terminal normal mode')
-keymap('t', "<C-;><C-;>", '<C-\\><C-n>', 'Terminal normal mode')
+keymap('t', '<C-;><C-;>', '<C-\\><C-n>', 'Terminal normal mode')
+keymap('t', '<C-w>', '<C-\\><C-n><C-w>')
 
 local function executor()
   if vim.bo.filetype == 'lua' then
