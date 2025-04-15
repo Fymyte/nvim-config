089d37fb9fcf20961181422da81bdc065e3483a1
Pierrick Guillaume <pguillaume@fymyte.com>
2025-04-15 21:33:39 +0200

    chore(globals): remove unused functions

--- lua/fymyte/globals.lua
+++ lua/fymyte/globals.lua
@@ -4,18 +4,9 @@
 
 --- Identity function, used to debug a value.
 --- This is usefull to easily print the content of a table
---- @param v any Any variable which can be printed
+---@param v any Any variable which can be printed
 ---@return any v
 P = function(v)
   print(vim.inspect(v))
   return v
-end
-
-RELOAD = function(name, starts_with_only)
-  return require('plenary.reload').reload_module(name, starts_with_only)
-end
-
-R = function(name)
-  RELOAD(name)
-  return require(name)
 end
