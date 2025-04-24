92812fa8f882f9477481b96afc5ec3df393be71e
Pierrick Guillaume <pguillaume@fymyte.com>
2025-04-24 09:49:43 +0200

    ref(unimpaired): qflist loclist loop back

--- lua/fymyte/plugins/tpope-magic.lua
+++ lua/fymyte/plugins/tpope-magic.lua
@@ -7,7 +7,17 @@ return {
   -- Operations on words
   'tpope/vim-abolish',
   -- ][ danse
-  { 'tpope/vim-unimpaired', event = 'VeryLazy' },
+  {
+    'tpope/vim-unimpaired',
+    event = 'VeryLazy',
+    config = function(_, _)
+      -- Make quickfix and location lists mapping loop
+      vim.keymap.set('n', '[q', '<cmd>try | cprev | catch | clast | endtry<cr>', { noremap = true })
+      vim.keymap.set('n', ']q', '<cmd>try | cnext | catch | cfirst| endtry<cr>', { noremap = true })
+      vim.keymap.set('n', '[l', '<cmd>try | lprev | catch | llast | endtry<cr>', { noremap = true })
+      vim.keymap.set('n', ']l', '<cmd>try | lnext | catch | lfirst| endtry<cr>', { noremap = true })
+    end,
+  },
   -- Builtin basic shell commands
   'tpope/vim-eunuch',
 }
