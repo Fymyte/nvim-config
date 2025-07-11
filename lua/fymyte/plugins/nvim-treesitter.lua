24ad2d0a10f8f2994d28ad19c9d0802b135ddfbf
Pierrick Guillaume <pguillaume@fymyte.com>
2025-07-11 15:05:32 +0200

    chore: use updated nvim-treesitter plugin

--- lua/fymyte/plugins/nvim-treesitter.lua
+++ lua/fymyte/plugins/nvim-treesitter.lua
@@ -1,17 +1,10 @@
 ---@type LazyPluginSpec
 return {
   'nvim-treesitter/nvim-treesitter',
+  branch = 'main',
+  lazy = false,
   build = function()
-    require('nvim-treesitter.install').update()
+    require('nvim-treesitter').update()
   end,
   version = false,
-
-  ---@type TSConfig
-  opts = {
-    auto_install = true,
-  },
-
-  config = function(_, opts)
-    require('nvim-treesitter.configs').setup(opts)
-  end,
 }
