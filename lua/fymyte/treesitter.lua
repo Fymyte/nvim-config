0134319a5697f8a2dcf170905d1f1e128b62970e
Pierrick Guillaume <pguillaume@fymyte.com>
2025-07-11 15:04:19 +0200

    chore: remove dead code

--- lua/fymyte/treesitter.lua
+++ lua/fymyte/treesitter.lua
@@ -5,8 +5,6 @@ local autocmd = vim.api.nvim_create_autocmd
 autocmd('FileType', {
   group = augroup 'tree-sitter-highlight',
   callback = function(args)
-    -- if vim.treesitter.language.get_lang(vim.bo[args.buf].filetype) ~= nil then
     pcall(vim.treesitter.start, args.buf)
-    -- end
   end,
 })
