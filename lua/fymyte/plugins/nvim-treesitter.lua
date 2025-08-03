e1b1ab4d3f313a7743fc5f9c5b7445ca1f3252a2
Pierrick Guillaume <pguillaume@fymyte.com>
2025-08-03 17:29:36 +0200

    ref(treesitter): enable syntax highlighting as backup
    If vim.treesitter.start fails, keep regular regex highlight.
    Also try to install the parser from nvim-treesitter before enabling
    tree-sitter highlight.

--- lua/fymyte/plugins/nvim-treesitter.lua
+++ lua/fymyte/plugins/nvim-treesitter.lua
@@ -1,9 +1,21 @@
+-- Make sure builtin parser are managed by nvim-treesitter as well so they receive updates as well
+local builtin_parsers = {
+  'c',
+  'lua',
+  'vim',
+  'vimdoc',
+  'markdown',
+  'markdown_inline',
+  'query',
+}
+
 ---@type LazyPluginSpec
 return {
   'nvim-treesitter/nvim-treesitter',
   branch = 'main',
   lazy = false,
   build = function()
+    require('nvim-treesitter').install(builtin_parsers)
     require('nvim-treesitter').update()
   end,
   version = false,
--- lua/fymyte/treesitter.lua
+++ lua/fymyte/treesitter.lua
@@ -1,10 +1,36 @@
 local augroup = require('fymyte.utils').augroup
 local autocmd = vim.api.nvim_create_autocmd
 
+local function try_install_parser(lang)
+  local ok, nvimts = pcall(require, 'nvim-treesitter')
+  if not ok then
+    return false
+  end
+
+  local languages = nvimts.get_available()
+  for _, v in pairs(languages) do
+    if v == lang then
+      nvimts.install(lang):wait(5000)
+      return true
+    end
+  end
+
+  return false
+end
+
 -- Automatically enable treesitter highlighting when opening a file
 autocmd('FileType', {
   group = augroup 'tree-sitter-highlight',
   callback = function(args)
-    pcall(vim.treesitter.start, args.buf)
+    local lang = vim.treesitter.language.get_lang(args.match)
+    -- If available in nvim-treesitter, install the parser
+    try_install_parser(lang)
+
+    -- Make sure that if it fails to activate tree-sitter highlighting, at
+    -- least there is old school regex syntax enabled
+    local ok = pcall(vim.treesitter.start, args.buf, lang)
+    if not ok then
+      vim.bo[args.buf].syntax = 'on'
+    end
   end,
 })
