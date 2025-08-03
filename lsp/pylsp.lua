646639376fc4975189b1881d129c97106dc3753a
Pierrick Guillaume <pguillaume@fymyte.com>
2025-08-03 17:32:35 +0200

    chore(lsp): use global settings of lspconfig for pylsp
    Enable isort by default

--- lsp/pylsp.lua
+++ lsp/pylsp.lua
@@ -1,22 +1,10 @@
 ---@type vim.lsp.Config
 return {
-  cmd = { 'pylsp' },
-  filetypes = { 'python' },
-
-  root_markers = {
-    '.git',
-    'pyproject.toml',
-    'setup.py',
-    'setup.cfg',
-    'requirements.txt',
-    'ruff.toml',
-    '.ruff.toml',
-  },
-
   settings = {
     pylsp = {
       plugins = {
         mypy = { enabled = true },
+        isort = { enabled = true },
         pycodestyle = { enabled = false },
         pyflakes = { enabled = false },
       },
