e7fada798a168c53adc49a6579904bfd2f19ef41
Pierrick Guillaume <pguillaume@fymyte.com>
2025-10-29 18:34:42 +0100

    feat: add ltex-utils plugin
    Add ltex lsp tools to update user dictionnary using ltex-ls.

---
+++ lua/fymyte/plugins/ltex-utils.lua
@@ -0,0 +1,12 @@
+return {
+  'jhofscheier/ltex-utils.nvim',
+  dependencies = {
+    'neovim/nvim-lspconfig',
+    'nvim-telescope/telescope.nvim',
+  },
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+  },
+}
