289c662b50807ee3ab85c3d18aae4eca1608a0ce
Pierrick Guillaume <pguillaume@fymyte.com>
2025-10-30 13:32:09 +0100

    feat: add better markdown rendering inside neovim
    Add conceal and extmarks to display custom elements like callouts,
    table, links... with rendering similar to web viewer.

---
+++ lua/fymyte/plugins/markdown-render.lua
@@ -0,0 +1,11 @@
+return {
+  'MeanderingProgrammer/render-markdown.nvim',
+  dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
+  ---@module 'render-markdown'
+  ---@type render.md.UserConfig
+  opts = {
+    completions = { lsp = { enabled = true } },
+    sign = { enabled = false },
+    quote = { repeat_linebreak = true },
+  },
+}
