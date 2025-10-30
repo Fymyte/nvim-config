e543abbc3d32bcf21c925c311dd08fa59b587f91
Pierrick Guillaume <pguillaume@fymyte.com>
2025-10-30 13:30:54 +0100

    feat: enable inline image preview using kitty protocol
    Note that it currently does not work when using zellij.

---
+++ lua/fymyte/plugins/image.lua
@@ -0,0 +1,8 @@
+return {
+  '3rd/image.nvim',
+  build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
+  opts = {
+    backend = 'kitty',
+    processor = 'magick_cli',
+  },
+}
