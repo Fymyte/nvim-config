5d79cf37e6668bb3cd11130a39d743fec97a81a9
Pierrick Guillaume <pguillaume@fymyte.com>
2025-11-07 09:03:38 +0100

    feat(colorizer): enable css parser in css, scss and html filetypes

--- lua/fymyte/plugins/colorizer.lua
+++ lua/fymyte/plugins/colorizer.lua
@@ -1,14 +1,22 @@
 ---@type LazyPluginSpec
 return {
   'catgoose/nvim-colorizer.lua',
-  event = 'BufReadPre',
+  -- event = 'BufReadPre',
   ---@module "colorizer.config"
   opts = {
     user_default_options = {
       names = false,
+      sass = { enable = true, parsers = { 'css' } },
       mode = 'virtualtext',
       virtualtext = '',
       virtualtext_inline = true,
+    },
+
+    filetypes = {
+      '*',
+      css = { css = true },
+      scss = { css = true },
+      html = { css = true },
     },
   },
 }
