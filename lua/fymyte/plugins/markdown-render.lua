9718539b2abb06f925a5f43755d95862e242f2fe
Pierrick Guillaume <pguillaume@fymyte.com>
2025-11-07 08:59:15 +0100

    feat: add markdown-render plugin
    Display markdown with some niceties for the eyes.

--- lua/fymyte/plugins/markdown-render.lua
+++ lua/fymyte/plugins/markdown-render.lua
@@ -1,11 +1,14 @@
 return {
-  'MeanderingProgrammer/render-markdown.nvim',
-  dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
-  ---@module 'render-markdown'
-  ---@type render.md.UserConfig
-  opts = {
-    completions = { lsp = { enabled = true } },
-    sign = { enabled = false },
-    quote = { repeat_linebreak = true },
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {
+      completions = { lsp = { enabled = true } },
+      sign = { enabled = false },
+      quote = { repeat_linebreak = true },
+    },
+    enabled = true,
   },
 }
