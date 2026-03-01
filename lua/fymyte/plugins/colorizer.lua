7c9bcbf38640a877e68c977d1224b7380309db80
Pierrick Guillaume <pguillaume@fymyte.com>
2026-03-01 17:16:11 +0100

    fix(colorizer): use new config schema

--- lua/fymyte/plugins/colorizer.lua
+++ lua/fymyte/plugins/colorizer.lua
@@ -1,15 +1,28 @@
 ---@type LazyPluginSpec
 return {
   'catgoose/nvim-colorizer.lua',
-  -- event = 'BufReadPre',
   ---@module "colorizer.config"
   opts = {
-    user_default_options = {
-      names = false,
-      sass = { enable = true, parsers = { 'css' } },
-      mode = 'virtualtext',
-      virtualtext = '',
-      virtualtext_inline = true,
+    ---@type colorizer.NewOptions
+    options = {
+      parsers = {
+        names = {
+          enable = false,
+          lowercase = false,
+          camelcase = false,
+          uppercase = false,
+          strip_digits = false,
+          custom = false,
+        },
+        sass = { enable = true, parsers = { 'css' } },
+      },
+      display = {
+        mode = 'virtualtext',
+        virtualtext = {
+          char = '',
+          position = 'after',
+        },
+      },
     },
 
     filetypes = {
