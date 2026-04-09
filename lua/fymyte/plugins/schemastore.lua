89ea930d62b327171e4264b93bea80e54a298077
Pierrick Guillaume <pguillaume@fymyte.com>
2026-04-09 22:48:33 +0200

    refactore: move jsonls and yaml-ls config to schemastore plugin config
    Because they both depends on the list of schema provided by the schemastore
    plugin, update their config here.

--- lua/fymyte/plugins/schemastore.lua
+++ lua/fymyte/plugins/schemastore.lua
@@ -1,4 +1,25 @@
 ---@type LazyPluginSpec
 return {
   'b0o/SchemaStore.nvim',
+  config = function()
+    vim.lsp.config('jsonls', {
+      settings = {
+        json = {
+          schemas = require('schemastore').json.schemas(),
+          validate = { enable = true },
+        },
+      },
+    })
+
+    vim.lsp.config('yaml-ls', {
+
+      settings = {
+        yaml = {
+          schemaStore = { enable = false, url = '' },
+          schemas = require('schemastore').yaml.schemas(),
+          customTags = { '!reference sequence' },
+        },
+      },
+    })
+  end,
 }
