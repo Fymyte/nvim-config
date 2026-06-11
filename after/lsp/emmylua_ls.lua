db36f8f62697fcc6295a1f5d2bd207fb68f57aa1
Pierrick Guillaume <pguillaume@fymyte.com>
2026-06-11 11:28:55 +0200

    fix(lsp): properly setup emmylua_ls

--- after/lsp/emmylua_ls.lua
+++ after/lsp/emmylua_ls.lua
@@ -1,15 +1,31 @@
+---@type vim.lsp.Config
 return {
+  on_init = function(client)
+    -- If the workspace has its own emmylua_ls/lua_ls config file, defer to it.
+    if client.workspace_folders then
+      local path = client.workspace_folders[1].name
+      if
+        path ~= vim.fn.stdpath 'config'
+        and (vim.uv.fs_stat(path .. '/.emmyrc.json') or vim.uv.fs_stat(path .. '/.luarc.json'))
+      then
+        client.config.settings = {}
+      end
+    end
+  end,
   settings = {
-    Lua = {
-      runtime = {
-        version = 'LuaJIT',
-        requirePattern = { 'lua/?.lua', 'lua/?/init.lua' },
-      },
+    emmylua = {
+      -- Tell the server which Lua you're using (usually LuaJIT, for Neovim).
+      runtime = { version = 'LuaJIT' },
+      diagnostics = { globals = { 'vim' } },
+      -- Make the server aware of Neovim runtime files.
       workspace = {
         library = {
           vim.env.VIMRUNTIME,
-          unpack(vim.fn.split(vim.opt.rtp._value, ',')),
+          -- For LSP Settings Type Annotations: https://github.com/neovim/nvim-lspconfig#lsp-settings-type-annotations
+          vim.api.nvim_get_runtime_file('lua/lspconfig', false)[1],
         },
+        -- Or pull in all of 'runtimepath'. May be slower! https://github.com/neovim/nvim-lspconfig/issues/3189
+        -- library = vim.api.nvim_get_runtime_file('', true),
       },
     },
   },
