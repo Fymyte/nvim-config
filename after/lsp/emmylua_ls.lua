b8a9d8908be2d56fd54018ae493acccff6756997
Pierrick Guillaume <pguillaume@fymyte.com>
2026-07-01 08:20:55 +0200

    feat(lsp): make emmylua aware of all plugins as well

--- after/lsp/emmylua_ls.lua
+++ after/lsp/emmylua_ls.lua
@@ -18,15 +18,7 @@ return {
       runtime = { version = 'LuaJIT' },
       diagnostics = { globals = { 'vim' } },
       -- Make the server aware of Neovim runtime files.
-      workspace = {
-        library = {
-          vim.env.VIMRUNTIME,
-          -- For LSP Settings Type Annotations: https://github.com/neovim/nvim-lspconfig#lsp-settings-type-annotations
-          vim.api.nvim_get_runtime_file('lua/lspconfig', false)[1],
-        },
-        -- Or pull in all of 'runtimepath'. May be slower! https://github.com/neovim/nvim-lspconfig/issues/3189
-        -- library = vim.api.nvim_get_runtime_file('', true),
-      },
+      workspace = { library = vim.api.nvim_get_runtime_file('', true) },
     },
   },
 }
